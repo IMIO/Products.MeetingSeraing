8cf37ba4e25dc0ab449cac3a32e80f686e4b1eda
André Nuyens <andre.nuyens@imio.be>
2015-04-28 15:34:14 +0000

    last change before training ?

--- src/Products/MeetingSeraing/model/generate.sh
+++ src/Products/MeetingSeraing/model/generate.sh
@@ -6,10 +6,10 @@ echo "Removing 'generatedsubscribers.zcml' include from configure.zcml"
 #we remove the eleventh line : <include file="generatedsubscribers.zcml"/>
 sed '/generatedsubscribers.zcml/d' ../configure.zcml >> ../tmp.zcml
 # make workflow removed before re-adding them so wfAdaptations are ok, see #7692
-sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingitemcouncilSeraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
-sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingitemcollegeSeraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
-sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingcouncilSeraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
-sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingcollegeSeraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
+sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingitemcouncilseraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
+sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingitemcollegeseraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
+sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingcouncilseraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
+sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <object name="meetingcollegeseraing_workflow" meta_type="Workflow" remove="True"/>' ../profiles/default/workflows.xml
 sed -i '/<object name="portal_workflow" meta_type="Plone Workflow Tool">/a \ <!-- first remove then re-apply so workflows are correct regarding workflow adaptations... -->' ../profiles/default/workflows.xml ../configure.zcml
 mv ../tmp.zcml ../configure.zcml
 rm ../generatedsubscribers.zcml
