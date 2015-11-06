33d54d101a44b25c577d49a9e51a691f47f5a377
André Nuyens <andre.nuyens@imio.be>
2015-11-06 13:24:48 +0000

    backported #12192 decision annex was not copied when duplicated an item

--- src/Products/MeetingSeraing/events.py
+++ src/Products/MeetingSeraing/events.py
@@ -12,9 +12,21 @@
 __author__ = """Andre NUYENS <a.nuyens@imio.be>"""
 __docformat__ = 'plaintext'
 
+from imio.actionspanel.utils import unrestrictedRemoveGivenObject
+from Products.PloneMeeting.interfaces import IAnnexable
 
-def onItemTransition(item, event):
+
+def onItemAfterTransition(item, event):
     '''Called whenever a transition has been fired on an item.'''
     if not event.transition or (item != event.object):
         return
     item.adapted().updatePowerEditorsLocalRoles()
+
+
+def onItemDuplicated(original, event):
+    '''After item's cloning, we removed decision annexe.
+    '''
+    newItem = event.newItem
+    # Delete the decision annexes that have been copied.
+    for annex in IAnnexable(newItem).getAnnexes(relatedTo='item_decision'):
+        unrestrictedRemoveGivenObject(annex)
