8c38e6298adb6921c094cbe03d20b5750faafa0e
André NUYENS <andre.nuyens@imio.be>
2020-01-07 15:24:03 +0100

    remove inused import

--- src/Products/MeetingSeraing/migrations/migrate_to_4_1.py
+++ src/Products/MeetingSeraing/migrations/migrate_to_4_1.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from Products.MeetingCommunes.migrations.migrate_to_4_1 import Migrate_To_4_1 as MCMigrate_to_4_1
-from Products.PloneMeeting.migrations.migrate_to_4100 import Migrate_To_4100
-from Products.PloneMeeting.migrations.migrate_to_4101 import Migrate_To_4101
-from Products.PloneMeeting.migrations.migrate_to_4102 import Migrate_To_4102
-from Products.PloneMeeting.migrations.migrate_to_4103 import Migrate_To_4103
-from Products.PloneMeeting.migrations.migrate_to_4104 import Migrate_To_4104
 
 import logging
 
@@ -23,13 +18,6 @@ class Migrate_To_4_1(MCMigrate_to_4_1):
 
         # call steps from Products.MeetingCommunes
         super(Migrate_To_4_1, self).run(extra_omitted=extra_omitted)
-
-        # execute upgrade steps in PM that were added after main upgrade to 4.1
-        Migrate_To_4100(self.portal).run()
-        Migrate_To_4101(self.portal).run(from_migration_to_41=True)
-        Migrate_To_4102(self.portal).run()
-        Migrate_To_4103(self.portal).run()
-        Migrate_To_4104(self.portal).run()
 
         # now MeetingCommunes specific steps
         logger.info('Migrating to MeetingSeraing 4.1...')
