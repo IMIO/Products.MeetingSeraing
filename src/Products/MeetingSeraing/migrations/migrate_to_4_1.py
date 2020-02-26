1d6c1258565b7abe32306c5459b4c4fa061394cb
André NUYENS <andre.nuyens@imio.be>
2020-02-26 13:15:41 +0100

    adapt migration step

--- src/Products/MeetingSeraing/migrations/migrate_to_4_1.py
+++ src/Products/MeetingSeraing/migrations/migrate_to_4_1.py
@@ -1,35 +1,25 @@
 # -*- coding: utf-8 -*-
 
+from Products.GenericSetup.tool import DEPENDENCY_STRATEGY_NEW
 from Products.MeetingCommunes.migrations.migrate_to_4_1 import Migrate_To_4_1 as MCMigrate_to_4_1
-
-import logging
-
-
-logger = logging.getLogger('MeetingSeraing')
 
 
 class Migrate_To_4_1(MCMigrate_to_4_1):
 
-    def run(self,
-            profile_name=u'profile-Products.MeetingSeraing:default',
-            extra_omitted=[]):
-        # change self.profile_name that is reinstalled at the beginning of the PM migration
-        self.profile_name = profile_name
-
-        # call steps from Products.MeetingCommunes
-        super(Migrate_To_4_1, self).run(extra_omitted=extra_omitted)
-
-        # now MeetingCommunes specific steps
-        logger.info('Migrating to MeetingSeraing 4.1...')
+    def run(self):
+        # reapply the actions.xml of collective.iconifiedcategory
+        self.ps.runImportStepFromProfile('profile-collective.iconifiedcategory:default', 'actions')
+        super(Migrate_To_4_1, self).run(extra_omitted=['Products.MeetingSeraing:default'])
+        self.reinstall(profiles=[u'profile-Products.MeetingSeraing:default'],
+                       ignore_dependencies=True,
+                       dependency_strategy=DEPENDENCY_STRATEGY_NEW)
 
 
 # The migration function -------------------------------------------------------
 def migrate(context):
     """This migration function:
 
-       1) Reinstall Products.MeetingCommunes and execute the Products.PloneMeeting migration;
-       2) Define default values for 'contacts' directory.position_types;
-       3) Define default ftw.labels labels.
+       1) Execute Products.MeetingCommunes migration.
     """
     migrator = Migrate_To_4_1(context)
     migrator.run()
