8bb9bd2dcb48ad18949a6cdfe5dfd9f06c2a1acc
André NUYENS <andre.nuyens@imio.be>
2020-05-04 13:24:36 +0200

    removed unuseless override test

--- src/Products/MeetingSeraing/tests/testToolPloneMeeting.py
+++ src/Products/MeetingSeraing/tests/testToolPloneMeeting.py
@@ -22,53 +22,12 @@
 # 02110-1301, USA.
 #
 
-from collective.iconifiedcategory.utils import get_categorized_elements
 from Products.MeetingSeraing.tests.MeetingSeraingTestCase import MeetingSeraingTestCase
 from Products.MeetingCommunes.tests.testToolPloneMeeting import testToolPloneMeeting as mctt
 
 
 class testToolPloneMeeting(MeetingSeraingTestCase, mctt):
     '''Tests the ToolPloneMeeting class methods.'''
-
-    def test_pm_CloneItemWithAnnexes(self):
-        '''Clones a given item containing annexes in parent item folder.'''
-        self.changeUser('pmManager')
-        item1 = self.create('MeetingItem')
-        # Add one annex and one decision annex
-        annex1 = self.addAnnex(item1)
-        self.addAnnex(item1, relatedTo='item_decision')
-        self.assertFalse(annex1.to_print, None)
-        annex1.to_print = True
-        workingFolder = item1.getParentNode()
-        # clone copyAnnexes=True and copyDecisionAnnexes=False by default
-        clonedItem = item1.clone()
-        self.assertEquals(
-            set([item1, clonedItem]), set(workingFolder.objectValues('MeetingItem')))
-        # Check that the annexes have been cloned, too.
-        self.assertEqual(len(get_categorized_elements(clonedItem)), 1)
-        newAnnex = clonedItem.objectValues()[0]
-        self.assertEqual(newAnnex.portal_type, 'annex')
-        # to_print is kept as cfg.keepOriginalToPrintOfClonedItems is True by default
-        self.assertTrue(self.meetingConfig.getKeepOriginalToPrintOfClonedItems())
-        self.assertTrue(newAnnex.to_print)
-        newAnnexesUids = [annex.UID() for annex in clonedItem.objectValues()]
-        self.assertEquals(
-            [annex.UID() for annex in get_categorized_elements(clonedItem, result_type='objects')],
-            newAnnexesUids)
-        self.assertEquals(clonedItem.categorized_elements.keys(), newAnnexesUids)
-        self.assertEquals(len(clonedItem.categorized_elements), 1)
-        # Test that an item viewable by a different user (another member of the
-        # same group) can be pasted too if it contains things. item1 is viewable
-        # by pmCreator1 too. And Also tests cloning without annex copying.
-        self.changeUser('pmCreator1')
-        clonedItem2 = item1.clone(copyAnnexes=False)
-        self.assertEquals(len(clonedItem2.categorized_elements), 0)
-        self.assertEquals(set([clonedItem2]),
-                          set(clonedItem2.getParentNode().objectValues('MeetingItem')))
-
-        # test when not keeping decision annexes for SERAING
-        clonedItem3 = item1.clone(copyAnnexes=False, copyDecisionAnnexes=True)
-        self.assertEquals(len(clonedItem3.categorized_elements), 0)
 
 
 def test_suite():
