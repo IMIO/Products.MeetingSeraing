bea69ac38449a0b9d78367476a80725662e1f064
Antoine Duchêne <antoineduchene@icloud.com>
2025-08-20 13:40:29 +0200

    Fix an issue in SeraingInsertBarcodeView.may_insert_barcode

--- src/Products/MeetingSeraing/browser/overrides.py
+++ src/Products/MeetingSeraing/browser/overrides.py
@@ -14,23 +14,21 @@ from Products.CMFCore.utils import _checkPermission
 
 class SeraingInsertBarcodeView(InsertBarcodeView):
     """ """
+
     def may_insert_barcode(self):
         """By default, must be (Meeting)Manager to include barcode and
            barcode must not be already inserted."""
         res = False
+        if self.tool.isManager(realManagers=True):
+            res = True
         if self.tool.getEnableScanDocs():
-            # bypass for 'Manager'
-            if self.tool.isManager(realManagers=True):
+            cfg = self.tool.getMeetingConfig(self.context)
+            # Special for MeetingSeraing: allow power editors to insert barcode
+            isPowerEditorEditable = hasattr(self.context.adapted(), "powerEditorEditable") and self.context.adapted().powerEditorEditable()
+            isManagerOrPowerEditor = self.tool.isManager(cfg) or isPowerEditorEditable
+            # is manager and no barcode already inserted and element still editable
+            if (isManagerOrPowerEditor or cfg.getAnnexEditorMayInsertBarcode()) and \
+               not self.context.scan_id and \
+               _checkPermission(ModifyPortalContent, self.context):
                 res = True
-            else:
-                cfg = self.tool.getMeetingConfig(self.context)
-                ctx = self.context
-                if IAnnex.providedBy(self.ctx):
-                    ctx = ctx.aq_parent
-                isManagerOrPowerEditor = self.tool.isManager(cfg) or ctx.adapted().powerEditorEditable()
-                barcode_inserted = getattr(self.context, "scan_id", False)
-                if isManagerOrPowerEditor and \
-                   not barcode_inserted and \
-                   _checkPermission(ModifyPortalContent, self.context):
-                    res = True
         return res
